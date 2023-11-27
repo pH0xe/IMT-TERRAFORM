6b1276f9934e324a1d4c8806b42a01c88d2d48f5
Julien Reig <julien.reig6@gmail.com>
2023-11-27 15:27:41 +0100

    add kube provider

--- partie2/providers.tf
+++ partie2/providers.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/google"
       version = "5.6.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.23.0"
+    }
   }
 }
 
@@ -12,4 +16,14 @@ provider "google" {
   region      = var.region
   zone        = var.zone
   credentials = file(var.credentials_file)
+}
+
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
+  token = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
+  )
 }
