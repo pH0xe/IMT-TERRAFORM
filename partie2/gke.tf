380626bab016598445d952e4d02949f4805a906c
Matteo Ordrenneau <71778601+Mattordre44@users.noreply.github.com>
2023-11-27 15:05:18 +0100

    add part2

---
+++ partie2/gke.tf
@@ -0,0 +1,22 @@
+variable "project_id" {
+  description = "project id"
+}
+
+variable "region" {
+  description = "region"
+}
+
+variable "zone" {
+  description = "zone"
+}
+
+resource "google_container_cluster" "mycluster" {
+  name     = "${var.project_id}-gke"
+  location = var.region
+
+  remove_default_node_pool = false
+  initial_node_count       = 1
+
+  network    = google_compute_network.myvpc.name
+  subnetwork = google_compute_subnetwork.mysubnet.name
+}
